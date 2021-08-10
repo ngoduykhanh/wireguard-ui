6490dd1fd095085675c4df94f534582469189658
Dan <shirtsdan@gmail.com>
2021-08-09 20:46:33 -0600

    Expand env vars and flags

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	rice "github.com/GeertJohan/go.rice"
@@ -16,12 +17,19 @@ import (
 	"github.com/ngoduykhanh/wireguard-ui/util"
 )
 
-// command-line banner information
 var (
+	// command-line banner information
 	appVersion = "development"
 	gitCommit  = "N/A"
 	gitRef     = "N/A"
 	buildTime  = fmt.Sprintf(time.Now().UTC().Format("01-02-2006 15:04:05"))
+	// configuration variables
+	flagDisableLogin   bool   = false
+	flagBindAddress    string = "0.0.0.0:5000"
+	flagSendgridApiKey string
+	flagEmailFrom      string
+	flagEmailFromName  string = "WireGuard UI"
+	flagSessionSecret  string
 )
 
 const (
@@ -34,18 +42,23 @@ const (
 )
 
 func init() {
-	// command-line flags
-	flagDisableLogin := flag.Bool("disable-login", false, "Disable login page. Turn off authentication.")
-	flagBindAddress := flag.String("bind-address", "0.0.0.0:5000", "Address:Port to which the app will be bound.")
+
+	// command-line flags and env variables
+	flag.BoolVar(&flagDisableLogin, "disable-login", LookupEnvOrBool("DISABLE_LOGIN", flagDisableLogin), "Disable login page. Turn off authentication.")
+	flag.StringVar(&flagBindAddress, "bind-address", LookupEnvOrString("BIND_ADDRESS", flagBindAddress), "Address:Port to which the app will be bound.")
+	flag.StringVar(&flagSendgridApiKey, "sendgrid-api-key", LookupEnvOrString("SENDGRID_API_KEY", flagSendgridApiKey), "Your sendgrid api key.")
+	flag.StringVar(&flagEmailFrom, "email-from", LookupEnvOrString("EMAIL_FROM_ADDRESS", flagEmailFrom), "'From' email address.")
+	flag.StringVar(&flagEmailFromName, "email-from-name", LookupEnvOrString("EMAIL_FROM_NAME", flagEmailFromName), "'From' email name.")
+	flag.StringVar(&flagSessionSecret, "session-secret", LookupEnvOrString("SESSION_SECRET", flagSessionSecret), "The key used to encrypt session cookies.")
 	flag.Parse()
 
 	// update runtime config
-	util.DisableLogin = *flagDisableLogin
-	util.BindAddress = *flagBindAddress
-	util.SendgridApiKey = os.Getenv("SENDGRID_API_KEY")
-	util.EmailFrom = os.Getenv("EMAIL_FROM")
-	util.EmailFromName = os.Getenv("EMAIL_FROM_NAME")
-	util.SessionSecret = []byte(os.Getenv("SESSION_SECRET"))
+	util.DisableLogin = flagDisableLogin
+	util.BindAddress = flagBindAddress
+	util.SendgridApiKey = flagSendgridApiKey
+	util.EmailFrom = flagEmailFrom
+	util.EmailFromName = flagEmailFromName
+	util.SessionSecret = []byte(flagSessionSecret)
 
 	// print app information
 	fmt.Println("Wireguard UI")
@@ -56,6 +69,10 @@ func init() {
 	fmt.Println("Git Repo\t:", "https://github.com/ngoduykhanh/wireguard-ui")
 	fmt.Println("Authentication\t:", !util.DisableLogin)
 	fmt.Println("Bind address\t:", util.BindAddress)
+	//fmt.Println("Sendgrid key\t:", util.SendgridApiKey)
+	fmt.Println("Email from\t:", util.EmailFrom)
+	fmt.Println("Email from name\t:", util.EmailFromName)
+	//fmt.Println("Session secret\t:", util.SessionSecret)
 
 	// initialize DB
 	err := util.InitDB()
@@ -109,4 +126,33 @@ func main() {
 	app.GET("/static/*", echo.WrapHandler(http.StripPrefix("/static/", assetHandler)))
 
 	app.Logger.Fatal(app.Start(util.BindAddress))
+}
+
+func LookupEnvOrString(key string, defaultVal string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return defaultVal
+}
+
+func LookupEnvOrBool(key string, defaultVal bool) bool {
+	if val, ok := os.LookupEnv(key); ok {
+		v, err := strconv.ParseBool(val)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "LookupEnvOrInt[%s]: %v\n", key, err)
+		}
+		return v
+	}
+	return defaultVal
+}
+
+func LookupEnvOrInt(key string, defaultVal int) int {
+	if val, ok := os.LookupEnv(key); ok {
+		v, err := strconv.Atoi(val)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "LookupEnvOrInt[%s]: %v\n", key, err)
+		}
+		return v
+	}
+	return defaultVal
 }
