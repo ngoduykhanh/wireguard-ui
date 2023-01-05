68049b58bcd3e9101b2541ed4c786a4c0366372d
Arminas <armisss4@gmail.com>
2023-01-05 18:44:44 +0200

    Fixed highlighting
    Fixed about page not being highlighted in menu when selected

--- handler/routes.go
+++ handler/routes.go
@@ -812,6 +812,9 @@ func ApplyServerConfig(db store.IStore, tmplBox *rice.Box) echo.HandlerFunc {
 // AboutPage handler
 func AboutPage() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.Render(http.StatusOK, "about.html", map[string]interface{}{})
+		return c.Render(http.StatusOK, "about.html", map[string]interface{}{
+			"baseData": model.BaseData{Active: "about", CurrentUser: currentUser(c)},
+		})
 	}
 }
+
